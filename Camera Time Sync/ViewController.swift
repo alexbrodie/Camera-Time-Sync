1315d53c3321d9df24a13fa52660bf8d21743ee4
Alex Brodie <abrodie77@hotmail.com>
2017-05-16 16:35:59 -0700

    Request GPS info Handle LocationManager notifications Refresh QR on a timer

--- Camera Time Sync/ViewController.swift
+++ Camera Time Sync/ViewController.swift
@@ -33,27 +33,26 @@ func getDateRfc3339() -> String {
     return formatter.string(from: Date())
 }
 
-func getQrMessage() -> String {
-    let date = getDateRfc3339()
-    var message = "{date:'\(date)'"
-    
-    if let loc = CLLocationManager().location {
-        message += ",lat=\(loc.coordinate.latitude),lon=\(loc.coordinate.longitude)"
-    }
-    
-    message += "}"
-    return message
-}
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var qr: UIImageView!
     @IBOutlet weak var qrText: UILabel!
     
+    var locMgr: CLLocationManager!
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        setQr() // what's a nib?
+
+        // Start getting GPS info
+        initLocMgr()
+        
+        // Set initial output
+        setQr()
+        
+        // Refresh periodically to keep displayed timestamp current
+        timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { [weak self] (timer:Timer) in self!.setQr() })
     }
 
     override func didReceiveMemoryWarning() {
@@ -61,12 +60,76 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func initLocMgr() {
+        // Reference: https://developer.apple.com/reference/corelocation/cllocationmanager#1669513
+        let status = CLLocationManager.authorizationStatus()
+        if (status != CLAuthorizationStatus.denied && status != CLAuthorizationStatus.restricted) {
+            let lm = CLLocationManager()
+            
+            lm.delegate = self
+            lm.desiredAccuracy = kCLLocationAccuracyBest
+            lm.distanceFilter = 100 * kCLLocationAccuracyKilometer
+            
+            if (status == CLAuthorizationStatus.notDetermined) {
+                lm.requestWhenInUseAuthorization()
+            }
+
+            // TODO?: lm.startMonitoring(for:)
+            // TODO?: lm.startMonitoringSignificantLocationChanges()
+
+            lm.requestLocation()
+
+            self.locMgr = lm
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // TODO: Error UI
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if (status != CLAuthorizationStatus.denied && status != CLAuthorizationStatus.restricted) {
+            manager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        setQr()
+    }
+    
+    func getQrMessage() -> String {
+        let date = getDateRfc3339()
+        var message = "{date:'\(date)'"
+        
+        if let loc = locMgr?.location {
+            message += ",lat=\(loc.coordinate.latitude),lon=\(loc.coordinate.longitude)"
+        }
+        
+        message += "}"
+        return message
+    }
+    
+    func getQrMessage2() -> String {
+        // For testing purposes, put info that's easy to verify that time/GPS/QR stuff is working
+        if let loc = locMgr?.location {
+            return "https://www.google.com/maps/@\(loc.coordinate.latitude),\(loc.coordinate.longitude),15z"
+        } else {
+            return "No location information available"
+        }
+    }
+
     func setQr() {
         let message = getQrMessage()
-        self.qrText.text = message
-        self.qr.image = makeQr(message: message, width: self.qr.bounds.width, height: self.qr.bounds.height)
+        if (self.qrText.text != message) {
+            self.qrText.text = message
+            self.qr.image = makeQr(message: message, width: self.qr.bounds.width, height: self.qr.bounds.height)
+        }
     }
 
 
 }
+
+
+
 
